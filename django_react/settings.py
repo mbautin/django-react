4c38355dfec10a3d92f93f186bd01b781d07ab24
Mikhail Bautin <mbautin@users.noreply.github.com>
2022-10-16 09:24:07 -0700

    Fini

--- django_react/settings.py
+++ django_react/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'leads.apps.LeadsConfig', # activate the new app
+
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -123,3 +125,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    )
+}
